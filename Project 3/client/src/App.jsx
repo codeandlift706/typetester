e86eea12cd19345c8399645250993ec3c48f9d29
Code Queen <channguyen97@gmail.com>
2023-11-02 14:11:39 -0700

    added login

--- Project 3/client/src/App.jsx
+++ Project 3/client/src/App.jsx
@@ -1,4 +1,14 @@
-import TestArea from './components/TestArea/TestArea'
+import { Outlet } from 'react-router-dom';
+import { StartBox } from './components/StartBox';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// import StartBox from './components/StartBox';
 
 function App() {
 
@@ -10,4 +20,5 @@ function App() {
   )
 }
 
-export default App
+
+export default App;
