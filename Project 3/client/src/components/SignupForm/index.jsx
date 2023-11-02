aafc0c55f6c8502c2ad5c8b2caef42d4620f83d5
Code Queen <channguyen97@gmail.com>
2023-11-02 13:24:15 -0700

    added to signup form

---
+++ Project 3/client/src/components/SignupForm/index.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from 'react';
+// import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations'
+
+function SignupForm(props) {
+    const [formState, setFormState] = useState({ firstName: '', lastName: '', username: '', email: '', password: '' });
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+                username: formState.username,
+                email: formState.email,
+                password: formState.password,
+            },
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // setUserFormData({ //reset the fields
+    //     firstName: '',
+    //     lastName: '',
+    //     username: '',
+    //     email: '',
+    //     password: '',
+    // });
+
+
+
+
+
+
+
+
+    return (
+        <>
+            {/* This is needed for the validation functionality above */}
+            {/* <Form noValidate validated={validated} onSubmit={handleFormSubmit}> */}
+            <Form onSubmit={handleFormSubmit}>
+                {/* show alert if server response is bad */}
+                {/* <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+                    Something went wrong with your signup!
+                </Alert> */}
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='username'>Username</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Your username'
+                        name='username'
+                        onChange={handleInputChange}
+                        value={userFormData.username}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Your email address'
+                        name='email'
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label htmlFor='password'>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                    disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+                    type='submit'
+                    variant='success'>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    );
+};
+
+export default SignupForm;
+
