68e2584b5f6d0e320cc1d779f0f1deea9103c22f
maliacho <maliacho21@gmail.com>
2023-11-02 14:17:08 -0700

    beginnings of NavBar.jsx- not complete

---
+++ Project 3/client/src/components/NavBar/index.jsx
@@ -0,0 +1,55 @@
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+
+    function showNavigation() {
+        if (Auth.loggedIn()) {
+            return (
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <Link to="/home">
+                            something
+                        </Link>
+                    </li>
+                    <li className="mx-1">
+                        <a href="/" onClick={() => Auth.logout()}>
+                            logout
+                        </a>
+                    </li>
+                </ul>
+            );
+        } else {
+            return (
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <Link to="/signup">
+                            signup
+                        </Link>
+                    </li>
+                    <li className="mx-1">
+                        <Link to="/login">
+                            login
+                        </Link>
+                    </li>
+                </ul>
+            );
+        }
+    }
+
+    return (
+        <header className="flex-row px-1">
+            <h1>
+                <Link to="/scoreboard">
+                    high scores
+                </Link>
+            </h1>
+
+            <nav>
+                {showNavigation()}
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
