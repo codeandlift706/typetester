b825d83038dd9207cb4c8e202598600086487101
moe-j22 <kmoe9262@gmail.com>
2023-11-03 10:52:57 -0700

    added content to main.jsx

---
+++ Project 3/client/src/main.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Score from './pages/Score';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: 'profile/:id',
+                element: <Profile />
+            },
+            {
+                path: 'score',
+                element: <Score/>
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  );
+
