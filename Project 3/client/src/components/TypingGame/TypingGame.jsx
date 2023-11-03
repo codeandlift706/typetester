978acc99de5ceb5e6c022c21d7bf57bfc529004f
n810tran <nathaniel.k.tran@gmail.com>
2023-11-03 08:32:22 -0700

    added logic to typingame.jsx. currently the user can type into the input field and matches will be console.logged with match or incorrect. input field moves across the provided prompt

--- Project 3/client/src/components/TypingGame/TypingGame.jsx
+++ Project 3/client/src/components/TypingGame/TypingGame.jsx
@@ -5,25 +5,55 @@ function TypingGame() {
     const prompts = ["hello world 1", "hello world 2", "hello world 3", "hello world 4", "hello world 5"]; // replace with API call to get prompts from database
 
     const [typingText, setTypingText] = useState('');
+    const [userInput, setUserInput] = useState('');
 
     const loadPrompt = () => {
         const randomPrompt = Math.floor(Math.random() * prompts.length);
-        const content = Array.from(prompts[randomPrompt]);
-        setTypingText(content);
+        const content = [...prompts[randomPrompt]];
+        setTypingText(content.join('')); // join the array of characters into a string
     }
+
+const handleInputChange = (event) => {
+    const userInput = event.target.value;
+    let isMatch = true;
+
+    for (let i = 0; i < userInput.length; i++) {
+        if (userInput[i] !== typingText[i]) {
+            isMatch = false;
+            break;
+        }
+    }
+
+    if (isMatch) {
+        console.log('Match!');
+    } else {
+        console.log('Wrong');
+    }
+
+    setUserInput(userInput);
+}
+
     useEffect(() => {
         loadPrompt();
     }, []);
+
+    const typedText = typingText.substring(0, userInput.length);
+    const remainingText = typingText.substring(userInput.length);
+
     return (
         <div className="container">
+            <span>{typedText}</span>
             <input
                 type="text"
                 className="input-field"
+                value={userInput}
+                onChange={handleInputChange} // call handleInputChange when the input field changes
             />
+            <span>{remainingText}</span>
             <TestArea
                 typingText={typingText}
             />
         </div>
     );
 };
-export default TypingGame
+export default TypingGame;
