c5eb5f10868c36f9b8ce11df05412889a65ddd8a
Code Queen <channguyen97@gmail.com>
2023-11-03 20:46:45 -0700

    worked a little on scores query and mutation

--- Project 3/client/src/components/Scoreboard/index.jsx
+++ Project 3/client/src/components/Scoreboard/index.jsx
@@ -1,4 +1,4 @@
-//scoreboard - displays list of: usernames & corresponding scores, button next to each score to remove
+//scoreboard - displays list of: usernames & corresponding scores, button next to each score to remove if it is your own score
 import { useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 
@@ -6,37 +6,29 @@ import { QUERY_SCORES, QUERY_USERS } from '../../utils/queries';
 import { REMOVE_SCORE } from '../../utils/mutations';
 
 
-//we need to pass in the user & the score ??????
 const Scoreboard = () => {
 
-    // const { loading, data } = useQuery(QUERY_SCORES);
-    // , { 
-    //     variables: { userId, score id?? }, //check ?? Not sure if need to pass in
-    //   });
+    const { loading, data } = useQuery(QUERY_SCORES);
 
-    const { loading, data } = useQuery(QUERY_USERS);
+    // const { loading, data } = useQuery(QUERY_USERS);
 
     const [removeScore, { error }] = useMutation(REMOVE_SCORE);
 
-    const scores = data?.scores || {}; //check if there's a data object and score property
+    const scores = data?.scores || []; //check if there's a data object and score property, if not, create an empty array
+    const user = data?.user || {};
 
-    const handleRemoveScore = async () => {
+    const handleRemoveScore = async (scoreId) => {
 
         try {
-            const { data } = await removeScore({
-                variables: { userId, scoreId } //remove score!!!! ????
-            })
-
+            await removeScore({
+                variables: { scoreId },
+            });
 
             // upon success, remove score id from localStorage???
-
         } catch (err) {
             console.error(err);
         }
     };
-
-
-
 
     if (!scores.length) { //if there are no scores
         return <h3>No Scores Yet</h3>;
@@ -49,19 +41,21 @@ const Scoreboard = () => {
     return ( //otherwise, return:
         <div>
             {scores &&
-                scores.map((score) => ( //map over each score, create a key so that react can identify that item being mapped over. Display each score
-                    <div key={score}>
+                scores.map((score) => ( //map over each score, create a key so that react can identify that item being mapped over
+                    <div key={score.id}>
                         <div>
                             <h4>
-                                {user.username}..........{score} <br />
+                                {user.username} .......... {score.score}
+                                {user.id === score.userId && ( //if the score's userid matches yours, the user's, display remove score button
+                                    <button onClick={() => handleRemoveScore(score.id)}>
+                                        Remove
+                                    </button>
+                                )}
                             </h4>
                         </div>
                     </div>
                 ))}
         </div>
-
-
-
     );
 };
 
