4de03e2da555d6696ad79a48df385b14d7525614
Code Queen <channguyen97@gmail.com>
2023-11-05 22:27:27 -0800

    only show profile link if not on profile

--- Project 3/client/src/components/NavBar/index.jsx
+++ Project 3/client/src/components/NavBar/index.jsx
@@ -3,22 +3,19 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     const isHomepage = window.location.pathname === "/";
+    const isProfile = window.location.pathname === "/profile/me";
 
     function showNavigation() {
         if (Auth.loggedIn()) {
             return (
                 <ul className="flex-row">
-                    <li className="mx-1">
-                        <Link to="/profile/me">
-                            Profile
-                        </Link>
-                    </li>
+                    {!isHomepage && <li className="mx-1"><Link to="/">Homepage</Link></li>}
+                    {!isProfile && <li className="mx-1"><Link to="/profile/me">Profile</Link></li>}
                     <li className="mx-1">
                         <a href="/" onClick={() => Auth.logout()}>
                             Logout
                         </a>
                     </li>
-                    {!isHomepage && <li className="mx-1"><Link to="/">Homepage</Link></li>}
                 </ul>
             );
         } else {
