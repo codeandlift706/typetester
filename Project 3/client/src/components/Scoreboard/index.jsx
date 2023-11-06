a757a66992e72f9cbd17f594da9887a136e11958
Code Queen <channguyen97@gmail.com>
2023-11-06 15:43:12 -0800

    cleaned up render score function

--- Project 3/client/src/components/Scoreboard/index.jsx
+++ Project 3/client/src/components/Scoreboard/index.jsx
@@ -1,4 +1,3 @@
-//scoreboard - displays list of: usernames & corresponding scores, button next to each score to remove if it is your own score
 import { useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 
@@ -9,45 +8,37 @@ import { REMOVE_SCORE } from '../../utils/mutations';
 const Scoreboard = () => {
 
     const { loading, data } = useQuery(QUERY_SCORES);
-    // const { loading, data } = useQuery(QUERY_USERS);
     const [removeScore, { error }] = useMutation(REMOVE_SCORE);
 
     const scores = data?.scores || []; //check if there's a data object and score property, if not, create an empty array
-    // const user = data?.user || {}; //check if there's a data object and user property, if not, create an empty array
 
-    // console.log(data.data.scores[0].score);
-    // const handleRemoveScore = async (scoreId) => {
+    const handleRemoveScore = async (scoreId) => {
 
-    //     try {
-    //         await removeScore({
-    //             variables: { scoreId },
-    //         });
+        try {
+            await removeScore({
+                variables: { scoreId },
+            });
 
-    //         // upon success, remove score id from localStorage???
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // };
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-    // if (!scores.length) { //if there are no scores
-    //     return <h3>No Scores Yet</h3>;
-    // }
 
-    for (let i = 0; i < scores.length; i++) {
-        const score = scores[i].score;
-        const username = scores[i].user.username;
-        // console.log(`Score: ${score}, Username: ${username}`);
+    if (!scores.length) {
+        return <h3>No Scores Yet</h3>;
     }
 
     if (loading) {
         return <h3>LOADING...</h3>;
     }
 
-    const scoreItems = scores.map((scoreObject) => {
-        const score = scoreObject.score;
-        const username = scoreObject.user.username;
+    const scoreItems = scores.map((scoreArray) => { //map over each score in the score array
+        const score = scoreArray.score; //grab the score from the score array
+        const username = scoreArray.user.username; //grab the username from the score array
         return (
-            <tr>
+            // render each pair as data in a table row
+            <tr> 
                 <td>{username}</td>
                 <td>{score}</td>
             </tr>
@@ -59,7 +50,7 @@ const Scoreboard = () => {
             <thead>
                 <tr>
                     <th>Username</th>
-                    <th>Score</th>
+                    <th>Score (WPM)</th>
                 </tr>
             </thead>
             <tbody>{scoreItems}</tbody>
